bf277f89aafa3283ab7093cd7fcf9bb1dd67bc8e
louizfs <louizfs@pop-os.localdomain>
2022-11-29 23:52:19 -0300

    done

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,8 @@
 server 'ec2-44-204-92-120.compute-1.amazonaws.com', user: 'ubuntu', roles: %w{web app db}
+set :pty, true
+
 set :ssh_options, {
 forward_agent: true,
 auth_methods: %w[publickey],
-keys: %w[/home/petal.pem]
+keys: %w[petal.pem]
 }
